f8dc19111cb976c92f9af5ca30b9250bfed24aae
JSH-data <super60447@gmail.com>
2024-07-30 10:43:27 +0900

    feat: add post interface

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHome(): string {
-    return 'Home Page';
-  }
-
-  @Get('post')
-  getPost(): string {
-    return 'Post Page';
-  }
-
-  @Get('user')
-  getUser(): string {
-    return 'User Page';
+  getHome(): Post {
+    return {
+      author: 'Seunghwa',
+      title: 'this is Post',
+      content: 'this is Post',
+      likeCount: 0,
+      commentCount: 0,
+    };
   }
 }
